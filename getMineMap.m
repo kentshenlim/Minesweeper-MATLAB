84bcaa4967244544597452ffc848586636f58260
kentshenlim <kentshen_lim24@hotmail.com>
2022-08-30 21:56:16 +0800

    Convert mine builder to function

---
+++ getMineMap.m
@@ -0,0 +1,17 @@
+function mineMap = getMineMap(x,y)
+% Build a mine map of x rows and y columns, 10 mines
+% Input: number of rows, then number of columns
+% Output: mine map, 0 no mine 1 mine
+mat = zeros(x,y);
+numberOfMine = 10; % Adjust later
+count = 0;
+while (count < numberOfMine)
+    r = randi([1,x]);
+    c = randi([1,y]);
+    if (mat(r,c) == 0)
+        mat(r,c) = 1;
+        count = count + 1;
+    end
+end
+mineMap = mat;
+end
