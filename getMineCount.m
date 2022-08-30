7b8c61806a7f4c5493ad6cef4f9dc45490c4fa91
kentshenlim <kentshen_lim24@hotmail.com>
2022-08-30 22:30:50 +0800

    Correct the index correcting condition

--- getMineCount.m
+++ getMineCount.m
@@ -8,20 +8,20 @@ for i = 1:r
     for j = 1:c
         lowR = i - 1; highR = i + 1; % Relevant row number for counting mines
         lowC = j - 1; highC = j + 1; % Relevant column number for counting mines
-        if lowR < 0 % Limit control, so not out of range
-            lowR = 0;
+        if lowR == 0 % Limit control, so not out of range
+            lowR = 1;
         end
         if highR > r
             highR = r;
         end
-        if lowC < 0
-            lowC = 0;
+        if lowC == 0
+            lowC = 1;
         end
         if highC > c
             highC = c;
         end
         fragment = mineMap(lowR:highR, lowC:highC);
-        numberOfMine = sum(fragment) - mineMap(i,j);
+        numberOfMine = sum(sum(fragment)) - mineMap(i,j);
         mineCount(i,j) = numberOfMine;
     end
 end
