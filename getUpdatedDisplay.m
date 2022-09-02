b32ea559fe90b83cb41272c220ccf58121b8f911
kentshenlim <kentshen_lim24@hotmail.com>
2022-09-02 21:40:13 +0800

    Correct losing condition

--- getUpdatedDisplay.m
+++ getUpdatedDisplay.m
@@ -1,11 +1,12 @@
-function updatedDisplay = getUpdatedDisplay(r, c, currentDisplay, mineCount)
+function updatedDisplay = getUpdatedDisplay(r, c, currentDisplay, mineMap, mineCount)
 % Compute and return displayed map after player choose a slot
 % Input: number of row then number of column then currentDisplay then mineCount
 % Output: latest map, 0 means closed, 1 means flipped
 % Space out, bounded by non-zero mine count
-currentMineCount = mineCount(r,c);
-if currentMineCount != 0
+isMine = mineMap(r,c);
+adjacentMine = mineCount(r,c);
+if isMine == 1
     currentDisplay(mineCount!=0) = 1; % Reveal mine location
     disp('You Lose!')
 else
-    
+
