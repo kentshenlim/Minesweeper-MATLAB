0052c30d396f3fa11746bb68d1672191e91b59e5
kentshenlim <kentshen_lim24@hotmail.com>
2022-09-29 21:26:03 +0100

    Calculate the dimension of a cell

---
+++ buildGrid.m
@@ -0,0 +1,17 @@
+function [] = buildGrid(r, c)
+% Build the grid!
+% Input: r = row number, c = column number
+% Output: none
+% Format of call: buildGrid(r, c)
+close all
+%% Main figure
+f = figure("Name", "Minesweeper-MATLAB", "Units", "normalized", ...
+    "Visible", "off", "Color", [.8, .8 ,.8]);
+movegui(f, "center")
+set(f, "Visible", "on")
+
+%% Grid cell dimension calculation
+widthCanva = 0.8;
+widthPerCell = widthCanva/c; % Also equal to height per cell
+heightCanva = widthPerCell*r;
+
